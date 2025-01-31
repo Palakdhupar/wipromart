00f63578f306243405d72448f8655489e1a1f435
Palak <palakdhupar4@gmail.com>
2025-01-31 12:53:59 +0530

    wipromart

---
+++ src/main/java/com/wipro/wipromart/service/CustomerService.java
@@ -0,0 +1,19 @@
+package com.wipro.wipromart.service;
+
+import java.util.List;
+
+import com.wipro.wipromart.entity.Customer;
+import com.wipro.wipromart.model.CustomerDto;
+
+public interface CustomerService {
+
+	CustomerDto saveCustomer(CustomerDto customerDto);
+	
+	CustomerDto getCustomerById(long customerId);
+	
+	List<CustomerDto> getAllCustomers();
+	
+   Customer updateCustomer(Customer customer);
+	
+	void deleteCustomer(long customerId);
+}
